edd60865bb210566baadb2d24fa8bdabcf08489c
Ishan1012 <ishandwivedi1210@gmail.com>
2025-09-05 13:27:53 +0530

    fix: removed header from profile page

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -1,11 +1,9 @@
-import Header from '@/components/Header'
 import UserProfilePage from '@/components/UserProfilePage'
 import React from 'react'
 
 function Profile() {
   return (
     <div>
-      <Header />
       <UserProfilePage />
     </div>
   )
