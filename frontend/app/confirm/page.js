d9fe265c731e27bfdc2db60613ba84f4a1f89ddb
Ishan1012 <ishandwivedi1210@gmail.com>
2025-09-07 10:30:11 +0530

    feat: add Confirm and Forgot Password pages with corresponding components

---
+++ frontend/app/confirm/page.js
@@ -0,0 +1,14 @@
+import CheckEmailPage from '@/components/CheckEmailPage'
+import Header from '@/components/Header'
+import React from 'react'
+
+function Confirm() {
+  return (
+    <div>
+      <Header />
+      <CheckEmailPage />
+    </div>
+  )
+}
+
+export default Confirm
