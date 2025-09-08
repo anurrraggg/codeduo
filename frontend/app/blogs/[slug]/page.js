fa224a0f6d57449e7a8df8108fd0ddca3a7164de
Ishan1012 <ishandwivedi1210@gmail.com>
2025-09-08 23:13:38 +0530

    feat: implement blog post page and related services with dynamic content

---
+++ frontend/app/blogs/[slug]/page.js
@@ -0,0 +1,16 @@
+import BlogPostPage from '@/components/BlogPostPage'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import React from 'react'
+
+function BlogPost() {
+    return (
+        <div>
+            <Header />
+            <BlogPostPage />
+            <Footer />
+        </div>
+    )
+}
+
+export default BlogPost
