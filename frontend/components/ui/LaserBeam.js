9290c17b2dc8eb4b3118198672f7e45ac322945f
Ishan1012 <ishandwivedi1210@gmail.com>
2025-09-14 01:54:31 +0530

    fix: fixed laser beam and movement of characters in quiz page

--- frontend/components/ui/LaserBeam.js
+++ frontend/components/ui/LaserBeam.js
@@ -1,7 +1,7 @@
 const LaserBeam = ({ style }) => {
     return (
         <div
-            className="h-1 bg-red-500 rounded-full shadow-[0_0_10px_red] z-[-10] hidden lg:block"
+            className="h-1 rounded-full shadow-[0_0_10px_red] z-[-20] hidden lg:block bg-gradient-to-r from-red-50 to-red-500"
             style={style}
         />
     );
