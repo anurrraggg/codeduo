b6de532d567b8c3a46c2f5cc7288f62bd73de05c
Ayush Raj <ayushraj.kanpur2004@gmail.com>
2025-09-26 14:35:56 +0530

    added routes in server js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,21 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+const attemptsRoutes = require('./routes/attemptsRoutes');
+const authRoutes = require('./routes/authRoutes');
+const badgesRoutes = require('./routes/badgesRoutes');
+const leaderboardRoutes =require('./routes/leaderboardRoutes');
+const lessonsRoutes = require('./routes/lessonsRoutes');
+const optionsRoutes = require('./routes/optionsRoutes');
+const questionsRoutes = require('./routes/questionsRoutes');
+const quizRoutes = require('./routes/quizRoutes');
+const userBadgesRoutes = require('./routes/userBadgesRoutes');
+
+
 dotenv.config();
 const app = express();
+
+
 
 // Middleware
 app.use(cors(
@@ -24,9 +37,20 @@ connectDB();
 // Routes
 app.get('/', (req, res) => res.send('API is running'));
 app.get('/api/test', (req, res) => res.send('check'));
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/quiz', require('./routes/quizRoutes'));
-app.use('/api/leaderboard', require('./routes/leaderboardRoutes'));
+
+app.use('/api/attempts', attemptsRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/badges',badgesRoutes);
+app.use('/api/leaderboard', leaderboardRoutes);
+app.use('/api/lessons',lessonsRoutes);
+app.use('/api/options',optionsRoutes);
+app.use('/api/questions',questionsRoutes);
+app.use('/api/quiz',quizRoutes);
+app.use('/api/user-badges',userBadgesRoutes);
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
