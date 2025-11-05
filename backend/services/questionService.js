cd6623d5ef2e15459d7c68c8e35a8340a3cbd549
anurrraggg <anuragpandey945028@gmail.com>
2025-11-05 17:41:11 +0530

    adada

--- backend/services/questionService.js
+++ backend/services/questionService.js
@@ -1,25 +1,33 @@
-const { questionRepository } = require("../repository/questionRepository")
+const { questionRepository } = require("../repository/questionRepository");
 
-export const questionService = {
-    createQuestion: async (questionData) => {
-        return await questionRepository.create(questionData);
-    },
-    findQuestionById: async (id) => {
-        return await questionRepository.findById(id);
-    },
-    findQuestionsByQuizId: async (quizId) => {
-        return await questionRepository.findByQuizId(quizId);
-    },
-    findQuestionByQuestionId: async (questionId) => {
-        return await questionRepository.findByQuestionId(questionId);
-    },
-    updateQuestion: async (questionId, questionData) => {
-        return await questionRepository.update(questionId, questionData);
-    },
-    findAllQuestions: async () => {
-        return await questionRepository.findAll();
-    },
-    deleteQuestion: async (id) => {
-        await questionRepository.delete(id);
-    },
+const questionService = {
+  createQuestion: async (questionData) => {
+    return await questionRepository.create(questionData);
+  },
+
+  findQuestionById: async (id) => {
+    return await questionRepository.findById(id);
+  },
+
+  findQuestionsByQuizId: async (quizId) => {
+    return await questionRepository.findByQuizId(quizId);
+  },
+
+  findQuestionByQuestionId: async (questionId) => {
+    return await questionRepository.findByQuestionId(questionId);
+  },
+
+  updateQuestion: async (questionId, questionData) => {
+    return await questionRepository.update(questionId, questionData);
+  },
+
+  findAllQuestions: async () => {
+    return await questionRepository.findAll();
+  },
+
+  deleteQuestion: async (id) => {
+    await questionRepository.delete(id);
+  },
 };
+
+module.exports = { questionService };
