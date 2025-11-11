f6d991551b311fd29e7dba5b0bcd2a4eeeb1beed
anurrraggg <anuragpandey945028@gmail.com>
2025-11-11 18:45:17 +0530

    added something on the first page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import LandingPage from "@/components/LandingPage";
+import LiveLoginTicker from "@/components/LiveLoginTicker";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <LandingPage />
+      <LiveLoginTicker />
       <Footer />
     </div>
   );
